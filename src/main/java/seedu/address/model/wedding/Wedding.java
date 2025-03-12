a8e535331abd2cb7c0af508288a6fd80bcc46fa6
Hrishikesh Sathyian <e1121418@u.nus.edu>
2025-03-12 20:14:06 +0800

    Add Date class

---
+++ src/main/java/seedu/address/model/wedding/Wedding.java
@@ -0,0 +1,9 @@
+package seedu.address.model.wedding;
+
+/**
+ * Represents a Wedding in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidName(String)}
+ */
+public class Wedding {
+
+}
