dcade1ecfda2498c518bfd26d35b3ac48fd96a8a
soonami69 <timothysoonwf@gmail.com>
2025-03-20 01:38:25 +0800

    Fix compile error in WeddingModelManager

--- src/main/java/seedu/address/model/WeddingModelManager.java
+++ src/main/java/seedu/address/model/WeddingModelManager.java
@@ -11,6 +11,7 @@ import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import seedu.address.commons.core.GuiSettings;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.UniquePersonList;
@@ -80,6 +81,7 @@ public class WeddingModelManager implements WeddingModel {
         requireNonNull(guiSettings);
         userPrefs.setGuiSettings(guiSettings);
     }
+
 
     @Override
     public Path getWeddingPlannerFilePath() {
