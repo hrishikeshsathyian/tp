cca121073915e4b3df0271d60a6335f041216cc2
Rae2480 <raeedareu@gmail.com>
2025-03-27 05:44:23 +0800

    Add sorting onto main logic of app

--- src/main/java/seedu/address/logic/parser/WeddingPlannerParser.java
+++ src/main/java/seedu/address/logic/parser/WeddingPlannerParser.java
@@ -16,6 +16,7 @@ import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
+import seedu.address.logic.commands.SortWeddingCommand;
 import seedu.address.logic.commands.wedding.AddWeddingCommand;
 import seedu.address.logic.commands.wedding.AddWeddingPersonCommand;
 import seedu.address.logic.commands.wedding.CloseWeddingCommand;
@@ -95,6 +96,9 @@ public class WeddingPlannerParser {
 
         case CloseWeddingCommand.COMMAND_WORD:
             return new CloseWeddingCommandParser().parse(arguments);
+
+        case SortWeddingCommand.COMMAND_WORD:
+            return new SortWeddingCommand();
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
