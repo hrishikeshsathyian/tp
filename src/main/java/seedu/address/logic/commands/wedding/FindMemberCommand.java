07c2d1f225210a37cbf31d25c4cfc225618db20f
许喆昊 <>
2025-03-27 01:13:23 +0800

    Fix check style

--- src/main/java/seedu/address/logic/commands/wedding/FindMemberCommand.java
+++ src/main/java/seedu/address/logic/commands/wedding/FindMemberCommand.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import java.util.ArrayList;
 import java.util.List;
 
-import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
 import seedu.address.logic.commands.exceptions.CommandException;
